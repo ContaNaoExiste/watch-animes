f26a8a57b3caaf5fa37f8ddd1f087ee7fdce98d1
André Victor <andre.victorueg@gmail.com>
2023-03-22 19:06:08 -0300

    Adicionado a parte de pesquisa dos animes

---
+++ js/anime-search.js
@@ -0,0 +1,45 @@
+function getAnimesBySearch(){
+    const query = new URLSearchParams(window.location.search).get("query")
+    if(query){
+        
+        $(".title-categorie").html(query)
+        $("#episodios-categories").html("")
+
+        fetch("https://watch-animes-api.vercel.app/search/"+ query).then(e => e.json())
+        .then(data=>{
+            if( data && data.animes){
+                data.animes.forEach(element => {
+                    $("#episodios-categories").append( getHtmlAnime(element))
+                });
+    
+                $('.set-bg').each(function () {
+                    var bg = $(this).data('setbg');
+                    $(this).css('background-image', 'url(' + bg + ')');
+                });
+            }
+        })
+    }
+}
+
+function getHtmlAnime(element){
+    return `
+    <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="product__item">
+            <div class="product__item__pic set-bg" data-setbg="${element.imdb.image}">
+            </div>
+            <div class="product__item__text">
+                <ul>
+                    ${ element.imdb.genre.split(",").map((genre)=>{ return "<li>" + genre + "</li>"})}
+                </ul>
+                <h5><a href="anime-details.html?id=${element.imdb.id}">${element.imdb.title}</a></h5>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+$( document ).ready(function() {
+    getAnimesBySearch()
+});
+
+
