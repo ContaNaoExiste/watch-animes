31b2cee171e09fe485a22b04acd3be1dcbf2c1e8
André Victor <andre.victorueg@gmail.com>
2023-07-27 16:34:03 -0300

    Alteração na forma de servir os arquivos, agora é usado o express

--- server.js
+++ server.js
@@ -44,9 +44,9 @@ function init() {
     initRoutes(app, "./routes") // views
     // initRoutes(app, "./api", "/api/") // api
 
-    /*app.listen(process.env.SERVER_PORT, () => {
+    app.listen(process.env.SERVER_PORT, () => {
         console.log(`Server UP on port ${process.env.SERVER_PORT}`)
-    })*/
+    })
 
     app.get('*', (req, res) => {
         res.status(404).send({message: 'Endpoint não configurado!', error: 404})
